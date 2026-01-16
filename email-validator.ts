3935e0e46c814e53b64deeab3e618f2ba5099a63
LOIC DE DEYN <loic.dedeyn@student.vinci.be>
2026-01-16 17:15:56 +0100

    refactor: emailValidator with simple return that includes all conditons

--- email-validator.ts
+++ email-validator.ts
@@ -1,6 +1,6 @@
 export default function emailValidator(email: string): boolean {
-    if (email.indexOf("@") === 0) return false;
     return email.includes("@")
         && email.includes(".") 
-        && !email.includes(" ");
+        && !email.includes(" ")
+        && email.indexOf("@") > 0;
 };
